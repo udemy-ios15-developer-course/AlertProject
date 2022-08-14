f7d5d58def02c9dcc724a7be69d825c0eb9cc28b
Chris Hand <cj.hand@gmail.com>
2022-08-14 09:19:02 -0700

    Built out most of this myself, jumping ahead of the instructor

---
+++ AlertProject/SuccessViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SuccessViewController.swift
+//  AlertProject
+//
+//  Created by Chris Hand on 8/14/22.
+//
+
+import UIKit
+
+class SuccessViewController: UIViewController {
+
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var passwordCheck: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signupClicked(_ sender: Any) {
+        var message = ""
+        if userName.text == "" {
+            message = "* The username cannot be blank\n"
+        }
+        
+        if password.text == "" {
+            message += "* The password cannot be blank\n"
+        }
+        
+        if password.text != passwordCheck.text {
+            message += "* The passwords do not match"
+        }
+        
+        if message != "" {
+            showAlert(message: message)
+            return
+        }        
+    }
+    
+    private func showAlert(message: String) {
+        let alert = UIAlertController(title: "Error!", message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "Try Again", style: .cancel) { _ in
+            print("button clicked")
+        }
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
+    
 }
 
